411b4041cfdfc93a8f00770579ba549f1536919a
joanpurba <joanpurba4@gmail.com>
2023-07-16 23:06:25 +0700

    start creating index page

--- index.php
+++ index.php
@@ -9,22 +9,27 @@ require_once "functions/mainPageHandler.php";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="style/style.css">
 </head>
-<style>
-  * {
-    margin: 0 !important;
-    padding: 0 !important;
-    box-sizing: border-box;
-    outline: none;
-  }
-
-  body {
-    height: 100vh !important;
-  }
-</style>
 
 <body>
-  <p>hello <?= $_SESSION["loginDatas"]["usersUsername"]; ?></p>
+  <header class="container-fluid d-flex justify-content-center align-items-center">
+    <section class="container-lg d-flex flex-row justify-content-between align-items-center">
+      <div>
+        <div>
+          <h2 class="fw-bold text-primary">App</h2>
+        </div>
+        <div>
+          <form action="" method="post">
+            <input type="text" name="test">
+          </form>
+        </div>
+      </div>
+      <nav>
+
+      </nav>
+    </section>
+  </header>
 </body>
 
 </html>
