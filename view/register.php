ed33e78256c3775649e7fee3ff99f195efb95660
joanpurba <joanpurba4@gmail.com>
2023-07-16 21:43:07 +0700

    change css file name

--- view/register.php
+++ view/register.php
@@ -10,7 +10,7 @@ require_once "../functions/registerFunction.php";
   <title>Register</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="../style/register.css">
+  <link rel="stylesheet" href="../style/form.css">
 </head>
 
 <body class="container-fluid d-flex justify-content-center align-items-center">
